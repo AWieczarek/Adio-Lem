55e7d66dc41d9918113cd97cd45741bf37ac9f1c
AWieczarek <adamwie12@gmail.com>
2021-07-06 17:45:19 +0200

    Add vote counter

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,6 +8,7 @@ using UnityEngine;
 public class GameManager : NetworkBehaviour
 {
     private GameObject myPlayerListItem;
+    private int voteCounter = 0;
 
     public override void NetworkStart()
     {
@@ -24,4 +25,16 @@ public class GameManager : NetworkBehaviour
             myPlayerListItem.GetComponent<NetworkObject>().SpawnWithOwnership(nc.Key);
         }
     }
+
+    public void CheckAllPlayersTrigger()
+    {
+        foreach (KeyValuePair<ulong, NetworkClient> nc in NetworkManager.Singleton.ConnectedClients)
+        {
+            PlayerController pc = nc.Value.PlayerObject.GetComponent<PlayerController>();
+            if (pc.playerTrigger.Value == 2) voteCounter += 1;
+        }
+        Debug.Log(voteCounter);
+        voteCounter = 0;
+    }
+
 }
